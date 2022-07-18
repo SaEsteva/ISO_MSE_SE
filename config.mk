6545f48e296457a821766040c34cae00682ed480
SaEsteva <44267876+SaEsteva@users.noreply.github.com>
2022-07-18 17:55:12 -0300

    OS inicial
    OS inicial con 2 tareas desde assembler

---
+++ config.mk
@@ -0,0 +1,23 @@
+# Compile options
+
+VERBOSE=n
+OPT=g
+USE_NANO=y
+SEMIHOST=n
+USE_FPU=y
+
+# Libraries
+
+USE_LPCOPEN=y
+USE_SAPI=n
+
+USE_FATFS=n
+
+USE_FREERTOS=n
+FREERTOS_HEAP_TYPE=5
+
+# Let FSSDC/FSUSB print their own status messages by DEBUGSTR()
+#DEFINES+=DEBUG_ENABLE
+DEFINES+=TICK_OVER_RTOS
+
+LOAD_INRAM=n
---
+++ inc/main.h
@@ -0,0 +1,40 @@
+/*
+ * main.h
+ *
+ *  Created on: 17 Jul 2022
+ *      Author: Santiago Esteva
+ */
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*==================[inclusions]=============================================*/
+
+/*==================[cplusplus]==============================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+
+/** @brief main function
+ * @return main function should never return
+ */
+int main(void);
+
+/*==================[cplusplus]==============================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _MAIN_H_ */
